ab97a89c70f99a62c5b0ccdd0c9e8beed87f3403
bekh0019 <dlp_abekh@ukr.net>
2018-11-09 19:18:12 +0200

    newcommit

--- src/main/java/com/jdbc/application/servlets/SubscriptionServlet.java
+++ src/main/java/com/jdbc/application/servlets/SubscriptionServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.SQLException;
-
+//hwfkwfbwkefbw
 public class SubscriptionServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
